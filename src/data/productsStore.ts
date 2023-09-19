5666b20f0ea9c8f6caba1920e21748abfe9e1d7c
jsonMartin <4033621+jsonMartin@users.noreply.github.com>
2023-09-18 17:25:08 -0700

    Disable buttons for disabled products Add Coming Soon section for disabled products Add new Upcoming Books UI: Change underline styling for homepage components

--- src/data/productsStore.ts
+++ src/data/productsStore.ts
@@ -26,11 +26,13 @@ export const products: any = {
     "release_date": "2023-08-15",
     "disabled": false,
   },
+
   "hunnie-bunnys-garden-mystery": {
     "id": "hunnie-bunnys-garden-mystery",
     "title": "Hunnie Bunny's Garden Mystery",
     "price": "20.99",
-    "description": `Hunnie Bunny’s Garden Mystery is a charming tale that takes young readers on an adventure with Hunnie Bunny and her woodland friends.\n It not only entertains, but also teaches valuable lessons about friendship, communication, and understanding. It will inspire children to develop strong relationships, and approach life’s challenges with a positive and compassionate mindset.\n Through colorful illustrations and engaging storytelling, this book captivates young minds and leaves them with a heartwarming message.  `, "release_date": "2023-08-15", "disabled": false,
+    "description": `Hunnie Bunny’s Garden Mystery is a charming tale that takes young readers on an adventure with Hunnie Bunny and her woodland friends.\n It not only entertains, but also teaches valuable lessons about friendship, communication, and understanding. It will inspire children to develop strong relationships, and approach life’s challenges with a positive and compassionate mindset.\n Through colorful illustrations and engaging storytelling, this book captivates young minds and leaves them with a heartwarming message.  `,
+    "release_date": "2023-09-30",
     "images": [{
       "title": 0,
       "src": "https://res.cloudinary.com/simple-reads-books/image/upload/w_1276,f_webp,q_auto/v1694474872/Hunnie_Bunnys_Garden_Mystery_Book_Cover_Front_tcnuvs.png"
@@ -51,8 +53,9 @@ export const products: any = {
       "title": 1,
       "src": "https://res.cloudinary.com/simple-reads-books/image/upload/w_1276,f_webp,q_auto/v1694474914/Hunnie_Bunnys_Garden_Mystery_Book_Cover_Back_hhjwwy.png"
     },
-
     ],
+    "type": "book",
+    "disabled": true,
   }
 }
 
--- src/pages/home/_ComingSoon.svelte
+++ src/pages/home/_ComingSoon.svelte
@@ -16,20 +16,20 @@
     <div class="flex flex-row gap-2.5 justify-center col-span-6 m-auto">
       <div class="flex flex-col gap-0 items-center justify-start">
         <div class="text-gray-900 text-center relative self-stretch">
-            <p class="text-xl md:text-2xl lg:text-3xl text-normal text-shadow-sm mb-5">Upcoming Books</p>
-            <div class="mt-30">
-              <span class="md:text-xl lg:text-2xl"
-                >Hunnie Bunny’s Garden Mystery
-                <br />
-                Hunnie Bunny’s Christmas Wish
-                <br />
-                Hunnie Bunny's Special Gift<br />
-                Hunnie Bunny's New Neighbors<br />
-                Hunnie Bunny and the Lost Fairy<br />
-                Hunnie Bunny and the Magic Flute<br />
-                Hunnie Bunny and the Unicorn<br />
-              </span>
-            </div>
+          <p class="text-xl md:text-2xl lg:text-3xl text-normal text-shadow-sm mb-5">Upcoming Books</p>
+          <div class="mt-30">
+            <span class="md:text-xl lg:text-2xl">
+              Hunnie Bunny’s Christmas Wish <br />
+              Hunnie Bunny's Special Gift<br />
+              Hunnie Bunny and the Lost Fairy<br />
+              Hunnie Bunny's New Neighbors<br />
+              Hunnie Bunny and the Magic Flute<br />
+              Hunnie Bunny and the Unicorn<br />
+              Hunnie Bunny and the Unicorn<br />
+              Hunnie Bunny's Great Escape<br />
+              Hunnie Bunny — A New Beginning<br />
+            </span>
+          </div>
         </div>
       </div>
     </div>
